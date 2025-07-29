d8c9fe556eaaf5da48f5fd13fddd6b96920615eb
Nearastro <destacuy16@gmail.com>
2025-07-29 16:46:08 +0700

    Update KEY.lua

--- KEY.lua
+++ KEY.lua
@@ -1,3 +1,4 @@
 NearKey-VIPHWID=3a4669a1-0baa-4ddb-b257-e414d516caea
 NearKey-VIPOWN=9d696234-e584-4365-9c95-c49d2efa5385
 NearKey-YanPler=6e86be71-afbd-4e49-9888-f1725b41fedb
+NearKey-LoganAsu=969d79bb-24da-444c-afa1-99f5b5d9f9c9
